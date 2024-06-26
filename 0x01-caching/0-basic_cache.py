6054cb74965cacedec1e5795c5300e3aeb924a79
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-06-26 18:38:39 +0300

    c

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,6 +7,11 @@ from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
     '''sdv s'''
+    def __init__(self):
+        """
+        Initialize the class using the parent class __init__ method
+        """
+        BaseCaching.__init__(self)
 
     def put(self, key, item):
         '''dsv'''
