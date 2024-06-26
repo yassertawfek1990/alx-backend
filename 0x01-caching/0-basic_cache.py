48579b0ba64f6bf967ed09862998d5e912c7069d
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-06-26 18:40:49 +0300

    c

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
-'''dsv'''
-
-
+""" dsv"""
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    '''sdv s'''
+    """dvd"""
+
     def __init__(self):
-        """
-        Initialize the class using the parent class __init__ method
-        """
+        """dv"""
         BaseCaching.__init__(self)
 
     def put(self, key, item):
-        '''dsv'''
-        if key is not None and item is not None:
+        """dv"""
+        if key is None or item is None:
+            pass
+        else:
             self.cache_data[key] = item
 
     def get(self, key):
-        '''rsd'''
-        return self.cache_data.get(key, None)
+        """dv"""
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
