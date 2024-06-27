032391e9ac82c208ed77a13a2093bc4119a4e0ae
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-06-27 15:41:49 +0300

    c

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
-'''fd f'''
+
+'''Task 0: Basic dictionary sdj asc awdqwm aesck
+'''
 
 
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    '''A cldfv'''
+    '''A cldfv wenclkwenclk eckkl clkwec '''
 
     def put(self, key, item):
-        '''dsvf'''
+        '''dsvf wjnkln cewkcklewvv welckm '''
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        '''rdsv'''
+        '''rdsv knlkwec klkewc m 
+        ewklckl e
+        ml;m;lm ewc'''
 
         return self.cache_data.get(key, None)
