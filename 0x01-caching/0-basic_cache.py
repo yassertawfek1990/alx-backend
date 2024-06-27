86776b1257e8f60a615c716330dc6a22b3e98a3e
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-06-27 15:44:50 +0300

    c

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,14 +10,22 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     '''A cldfv wenclkwenclk eckkl clkwec '''
 
+    def __init__(self):
+        """
+        Initialize the class ds s dsvusing the parent class __init__ method
+        """
+        BaseCaching.__init__(self)
+
     def put(self, key, item):
         '''dsvf wjnkln cewkcklewvv welckm '''
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        '''rdsv knlkwec klkewc m 
+        '''rdsv knlkwec klkewc m
         ewklckl e
         ml;m;lm ewc'''
 
-        return self.cache_data.get(key, None)
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
